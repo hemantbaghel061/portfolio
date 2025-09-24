e286e5680c3588d1b107fea9794feed3e630936a
Hemant Baghel <154806004+hemantbaghel061@users.noreply.github.com>
2025-09-24 15:30:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll to section smoothly
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
